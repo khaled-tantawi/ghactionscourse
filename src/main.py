80b4f782b6fba206e688eaf427a48b4d6b41463c
tantawikhaled <tantawikhaled02@gmail.com>
2026-03-24 10:36:21 +0100

    test new branch

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ def say_hello(name: str = "World") -> str:
     return f"Hello, {name.upper()}!"
 
 # ignore me 
-if __name__ == "__main__":
-    print(say_hello("Alice"))
+# if __name__ == "__main__":
+#     print(say_hello("Alice"))
